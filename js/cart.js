2ea4cf14b2e0d7a3a7d2df2933dfddcaf1f2e121
Pohuliai <daniilpoguliai15@gmail.com>
2023-03-22 15:42:01 +0200

    11

--- js/cart.js
+++ js/cart.js
@@ -1,30 +1,68 @@
+let order = {
+    "deliveryId": "FD713788-B5AE-49FF-8B2C-F311B9CB0CC4",
+    "paymentId": "FD713788-B5AE-49FF-8B2C-F311B9CB0CC4",
+    "customerName": "Ivan",
+    "customerPhone": "1234567890",
+    "customerMail": null,
+    "customerAddress": "2ihuwfeksndx",
+    "comment": null
+}
+
+let contents = []
+let cont = {
+    dishId:null,
+    orderId:null,
+    number: null
+}
+
 let empty = '<div class="empty-cart">Ваша корзина пуста <br> <a href="index.html"> На головну </a> </div>'
+let totalPrice = '<div class="total_price">Загальна сума: <span id="total_price">0</span> грн</div>'
 let cart_item_list = '<div class="cart-item-list"> </div>'
 
 $(document).ready(function () {
 
     if (JSON.parse(localStorage.getItem("cart")) == null) {
         cart = {}
+        //console.log($(".cart-item-list").length)
+        $(".total_price").hide()
+        $("#korzina").hide()
     } else {
         cart = JSON.parse(localStorage.getItem("cart"))
+        if ( Object.getOwnPropertyNames(cart).length == 0 ){
+            $(".total_price").hide()
+            $("#korzina").hide()
+            //console.log(cart.length, cart)
+        } else {
+        $(".empty-cart").remove()
+        $("#total_price").text("0")
+        $(".total_price").show()
+        $("#korzina").show()
         if (Object.keys(cart).length > 0) {
             //console.log(cart)
-            //console.log(Object.keys(cart))
+            console.log(Object.keys(cart))
             for (let key in cart) {
                 refreshCart(key)
             }
-        }
+        }}
     }
 
     window.onstorage = event => {
         console.log("update")
+        
         cart = JSON.parse(localStorage.getItem("cart"))
+        if(Object.getOwnPropertyNames(cart).length == 0){
+            $(".total_price").hide()
+            $("#korzina").hide()
+            $(".cart-item-list").append(empty)
+        } else {
         $(".cart-item-list").empty()
+        $(".total_price").show()
+        $("#korzina").show()
         //$(".cart-place").prepend(cart_item_list)
         for (let key in cart) {
-                refreshCart(key)
+            refreshCart(key)
         }
-        refreshPrice()
+        refreshPrice()}
     }
 
 
@@ -45,6 +83,7 @@ $(document).ready(function () {
             $("#total_price").text(currentPrice.toString())
             if (currentPrice <= 0) {
                 $(".total_price").hide()
+                $("#korzina").hide()
             }
             jQuery(this).parent().remove()
             if ($(".cart-item-list").find(".cart-item").length == 0) {
@@ -90,6 +129,74 @@ $(document).ready(function () {
         }
 
         return o.val(t), o.change(), !1
+    })), $("#delivery-method").change(function () {
+
+        if ($(this).val() == "Кур'єрська доставка") {
+            $('textarea[name="address"]').show()
+            $("#pickup-method").hide()
+        } else {
+
+            $('textarea[name="address"]').hide()
+            $("#pickup-method").show()
+        }
+    }), $(".btn-checkout").on("click", (function () {
+
+        if ($(".af_fullname").val() == '') {
+            $(".af_fullname").addClass("error")
+            return false
+        } else {
+            $(".af_fullname").removeClass("error")
+            order.customerName = $(".af_fullname").val()
+        }
+
+        if ($(".af_phone").val() == '') {
+            $(".af_phone").addClass("error")
+            return false
+        } else {
+            if (!validatePhone($(".af_phone").val())) {
+                $(".af_phone").addClass("error")
+                return false
+            }
+            $(".af_phone").removeClass("error")
+            order.customerPhone = $(".af_phone").val()
+        }
+
+        if (($('textarea[name="address"]').val() == '') && ($('textarea[name="address"]').css('display') != 'none')) {
+            $('textarea[name="address"]').addClass("error")
+            return false
+        } else {
+            $('textarea[name="address"]').removeClass("error")
+            order.customerAddress = $('textarea[name="address"]').val()
+        }
+
+
+        order.customerMail = $(".af_email").val()
+        order.comment = $('textarea[name="comment"]').val()
+        let cart = JSON.parse(localStorage.getItem("cart"))
+        let i = 0
+        for (let key in cart) {
+            // if (i == 0){
+            //     contents[0].dishId = key
+            //     contents[0].orderId = i
+            //     contents[0].number = cart[key].amount
+            // } else {
+            cont.dishId = key
+            cont.orderId = i
+            cont.number = cart[key].amount
+            contents.push(cont)
+            cont = {}
+            // }
+            i++
+            //console.log(key)
+        }
+        console.log(order)
+        console.log(contents)
+        return false
+
+
+
+        // console.log("pivo")
+        // return false
     }))
 
 })
@@ -110,7 +217,7 @@ function refreshAmount(id) {
     console.log(id)
     let cart = JSON.parse(localStorage.getItem("cart"))
     let amount = cart[id].amount
-    if (amount == 0){
+    if (amount == 0) {
         $(`div[data-id=${id}]`).remove()
     } else $(`div[data-id=${id}]`).find(".description").find(".item-count").find(".spinner").find("input").val(amount)
 }
@@ -124,4 +231,9 @@ function refreshPrice() {
         price = (amount * parseInt(priceText.substring(0, priceText.length - 2))) + price
     });
     $("#total_price").text(price.toString())
+}
+
+function validatePhone(phone) {
+    let re = /^[0-9\s]*$/;
+    return re.test(String(phone));
 }
